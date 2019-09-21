c593ac88ba8b8c13fa3ff1a6795d65a1450cbcf5
Mason Kim <mason@linux-qno5.fritz.box>
2019-09-22 01:56:14 +0200

    Use new form in home_page, simplify tests. NB breaks stuff

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -4,6 +4,7 @@ from django.test import TestCase
 from django.urls import resolve
 from django.utils.html import escape
 
+from lists.forms import ItemForm
 from lists.models import Item, List
 from lists.views import home_page
 
@@ -12,7 +13,13 @@ class HomePageTest(TestCase):
 
     def test_uses_home_template(self):
         response = self.client.get('/')
+
         self.assertTemplateUsed(response, 'home.html')
+
+    def test_home_page_uses_item_form(self):
+        response = self.client.get('/')
+
+        self.assertIsInstance(response.context['form'], ItemForm)
 
 
 class NewListTest(TestCase):
@@ -20,14 +27,15 @@ class NewListTest(TestCase):
     def test_can_save_a_POST_request(self):
         self.client.post(
             '/lists/new', data = {'item_text': 'A new list item'})
+
         self.assertEqual(Item.objects.count(), 1)
         new_item = Item.objects.first()
         self.assertEqual(new_item.text, 'A new list item')
 
     def test_redirects_after_POST(self):
         response = self.client.post(
-            '/lists/new', data = {'item_text': 'A new list item'}
-        )
+            '/lists/new', data = {'item_text': 'A new list item'})
+
         new_list = List.objects.first()
         self.assertRedirects(response, f'/lists/{new_list.id}/')
 
@@ -50,7 +58,9 @@ class ListViewTest(TestCase):
 
     def test_uses_list_template(self):
         list_ = List.objects.create()
+
         response = self.client.get(f'/lists/{list_.id}/')
+
         self.assertTemplateUsed(response, 'list.html')
 
     def test_passes_correct_list_to_template(self):
