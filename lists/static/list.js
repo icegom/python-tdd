089ae0ada438e7c435736b8b5e812a75eae282d1
Mason Kim <mason@linux-qno5.fritz.box>
2019-09-23 01:40:36 +0200

    Use namespace and onload boilerplate in base.html.

--- lists/static/list.js
+++ lists/static/list.js
@@ -1,3 +1,5 @@
+// window.Superlists = {};
+// window.Superlists.initialize = function () {
 var initialize = function () {
   $('input[name="text"]').on('keypress', function () {
     $('.has-error').hide();
