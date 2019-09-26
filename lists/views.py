8cf9f2419f087b1b166124296a4c1c580999bf84
Mason Kim <mason@linux-qno5.fritz.box>
2019-09-26 23:26:54 +0200

    new_list view tries to assign owner but cant

--- lists/views.py
+++ lists/views.py
@@ -1,8 +1,11 @@
+from django.contrib.auth import get_user_model
 from django.core.exceptions import ValidationError
 from django.shortcuts import redirect, render
 
 from lists.forms import ExistingListItemForm, ItemForm
 from lists.models import Item, List
+
+User = get_user_model()
 
 
 # Create your views here.
@@ -26,7 +29,9 @@ def view_list(request, list_id):
 def new_list(request):
     form = ItemForm(data=request.POST)
     if form.is_valid():
-        list_ = List.objects.create()
+        list_ = List()
+        list_.owner = request.user
+        list_.save()
         form.save(for_list=list_)
         return redirect(list_)
     else:
@@ -34,5 +39,7 @@ def new_list(request):
 
 
 def my_lists(request, email):
-    return render(request, 'my_lists.html')
+    owner = User.objects.get(email=email)
+
+    return render(request, 'my_lists.html', {'owner': owner})
 
