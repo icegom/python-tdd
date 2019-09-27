5b38d58e4056488eacf3e149ff3471dfee6f654b
Mason Kim <mason@linux-qno5.fritz.box>
2019-09-27 18:34:27 +0200

    Add list owners via forms. more isolated tests

--- functional_tests/test_my_lists.py
+++ functional_tests/test_my_lists.py
@@ -59,6 +59,7 @@ class MyListsTest(FunctionalTest):
         self.browser.find_element_by_link_text('My lists').click()
         self.wait_for(
             lambda: self.browser.find_element_by_link_text('Click cows'))
+        self.browser.find_element_by_link_text('Click cows').click()
         self.wait_for(
                 lambda: self.assertEqual(
                     self.browser.current_url, second_list_url))
